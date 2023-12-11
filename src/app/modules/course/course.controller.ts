a2a0736fef36323ac9d6b9f3f0eaadf72c0dcd1f
rakib8680 <rakib.cst.1st@gmail.com>
2023-12-11 23:01:38 +0600

    generic send response created

--- src/app/modules/course/course.controller.ts
+++ src/app/modules/course/course.controller.ts
@@ -1,5 +1,20 @@
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { courseServices } from "./course.service";
 
 
 
 // create course 
-const createCourse = 
+const createCourse = catchAsync(async (req, res) => {
+
+    const courseData = req.body;
+    const result = await courseServices.createCourse(courseData);
+
+    sendResponse(res,{
+        success: true,
+        statusCode: 201,
+        message: 'Course created successfully',
+        data: result
+    });
+
+});
